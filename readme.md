fe4259dfd63e21368d38d182b65b79509c57f61a
Pawel Jezior <paweljfrontend@gmail.com>
2021-09-15 21:40:02 +0200

    hydrapp

--- readme.md
+++ readme.md
@@ -1,18 +1,15 @@
-![cover](https://cotenfrontend.pl/img/cover.png)
+![cover](pwa/apple-icon-180.png)
 
-# WTF Webpack Starter Kit 
+# HYDRAPP
+PWA project, where I used:
+- HTML
+- CSS/SCSS
+- JS
+- NPM
+- Webpack
+## Run with:
+`npm install` and `npm run start` (localhost:8080)
+### You can find presentation [here](https://pawel-jezior.github.io/hydrapp/) on any device!
 
-Simple toolkit for your static websites development.
-
-## How to use it?
-
-Click on `Use this template` button to generate a new repository based on this starter kit. After that clone it to your computer, go to the project directory in console and type `npm install`.
-
-## Available scripts
-
-`npm run start` - runs development mode
-
-`npm run build` - runs build process for production
-
-`npm run publish` - runs build process and publish the page using `gh-pages` branch
+### Project base generated from wtf-webpack template.
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,42 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import { registerSW } from "./pwa.js";
+registerSW();
 
-/* place your code below */
+let counter = document.querySelector(".main__counter--js");
+const buttonAdd = document.querySelector(".main__button-add--js");
+const buttonSubtract = document.querySelector(".main__button-subtract--js");
 
-console.log('HELLO 🚀')
+const key = new Date().toISOString().slice(0, 10);
+
+console.log(key);
+console.log(counter.innerHTML);
+
+let counterNumber;
+
+if (!key) {
+  counterNumber = 0;
+  localStorage.setItem(key, counterNumber);
+  counter.innerHTML = `${counterNumber}`;
+  console.log("IF: " + counter.innerHTML);
+} else {
+  counterNumber = Number(localStorage.getItem(key));
+  counter.innerHTML = `${counterNumber}`;
+  console.log("ELSE: " + counter.innerHTML);
+}
+
+buttonAdd.addEventListener("click", () => {
+  console.log("+1");
+  counterNumber = counterNumber + 1;
+  localStorage.setItem(key, counterNumber);
+  counter.innerHTML = `${localStorage.getItem(key)}`;
+});
+
+buttonSubtract.addEventListener("click", () => {
+  if (counterNumber >= 1) {
+    console.log("-1");
+    counterNumber = counterNumber - 1;
+    localStorage.setItem(key, counterNumber);
+    counter.innerHTML = `${localStorage.getItem(key)}`;
+  }
+});
