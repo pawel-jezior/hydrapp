efa47c755c4b20c97c1cee74d6d38e8e3f40cf75
Pawel Jezior <paweljfrontend@gmail.com>
2021-09-16 18:30:41 +0200

    hydrapp v. 1.1

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ let counter = document.querySelector(".main__counter--js");
 const buttonAdd = document.querySelector(".main__button-add--js");
 const buttonSubtract = document.querySelector(".main__button-subtract--js");
 
-const key = new Date().toISOString().slice(0, 10);
+const key = new Date().toLocaleString().slice(0, 10);
 
 console.log(key);
 console.log(counter.innerHTML);
@@ -18,15 +18,12 @@ if (!key) {
   counterNumber = 0;
   localStorage.setItem(key, counterNumber);
   counter.innerHTML = `${counterNumber}`;
-  console.log("IF: " + counter.innerHTML);
 } else {
   counterNumber = Number(localStorage.getItem(key));
   counter.innerHTML = `${counterNumber}`;
-  console.log("ELSE: " + counter.innerHTML);
 }
 
 buttonAdd.addEventListener("click", () => {
-  console.log("+1");
   counterNumber = counterNumber + 1;
   localStorage.setItem(key, counterNumber);
   counter.innerHTML = `${localStorage.getItem(key)}`;
@@ -34,7 +31,6 @@ buttonAdd.addEventListener("click", () => {
 
 buttonSubtract.addEventListener("click", () => {
   if (counterNumber >= 1) {
-    console.log("-1");
     counterNumber = counterNumber - 1;
     localStorage.setItem(key, counterNumber);
     counter.innerHTML = `${localStorage.getItem(key)}`;
